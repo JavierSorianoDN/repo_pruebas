409e12fc2bb7ff72158c93715812922e3a40058e
JavierSorianoDN <javier.soriano@displaynote.com>
2023-12-31 17:49:40 +0100

    commit inicial

--- main.cpp
+++ main.cpp
@@ -4,23 +4,8 @@
 
 int main() {
 
-    std::cout << "rama develop commit 2 - linea primera" << std::endl;
+    std::cout << "commit inicial" << std::endl;
 
-    std::cout << "commit 5 - linea quinta" << std::endl;
-
-    std::cout << "rama develop commit 1 - linea primera" << std::endl;
-
-    std::cout << "rama develop commit 0 - linea inicial" << std::endl;
-
-    std::cout << "commit 4 - linea cuarta" << std::endl;
-
-    std::cout << "commit 3 - linea tercera" << std::endl;
-
-    std::cout << "commit 2 - linea segunda" << std::endl;
-
-    std::cout << "commit 1 - linea primera" << std::endl;
-
-    std::cout << "commit 0 - linea inicial" << std::endl;
     // Return 0 to indicate successful execution
     return 0;
 }
