db0dd489b0343a7c166f44cc543049c308006dc6
JavierSorianoDN <javier.soriano@displaynote.com>
2024-03-02 20:09:16 +0100

    probando git citool

--- main.cpp
+++ main.cpp
@@ -17,6 +17,11 @@ int main() {
 
     std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
 
+    std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
+
+    std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
+
+
     std::cout << "rama_poner_cout_nuevo" << std::endl;
 
     std::cout << "commit inicial" << std::endl;
