d2ce846696854724b7d39e3f8560538f043cae5e
JavierSorianoDN <javier.soriano@displaynote.com>
2024-03-11 18:09:48 +0100

    tercer commit rama: rama_poner_cout_nuevo

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main() {
 
     std::cout << "segundo commit en la rama rama_poner_cout_nuevo" << std::endl;
 
+    std::cout << "tercer commit en la rama rama_poner_cout_nuevo" << std::endl;
+
     // Return 0 to indicate successful execution
     return 0;
 }
