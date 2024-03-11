bb30c12f81853d54b7b936e3f5f5c61e3a22bd9b
JavierSorianoDN <javier.soriano@displaynote.com>
2024-03-11 18:03:29 +0100

    main commit inicial

--- main.cpp
+++ main.cpp
@@ -4,32 +4,7 @@
 
 int main() {
 
-    std::cout << "comprobacion fetch" << std::endl;
-
-    std::cout << "comprobacion citool2" << std::endl;
-
-
-    std::cout << "cambio 3 hecho desde una copia repo" << std::endl;
-
-    std::cout << "cambio 2 hecho desde una copia repo" << std::endl;
-
-    std::cout << "cambio hecho desde una copia repo" << std::endl;
-
-    std::cout << "cout hecho en la rama main" << std::endl;
-
-    std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
-
-    std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
-
-    std::cout << "segundo rama_poner_cout_nuevo" << std::endl;
-
-
-    std::cout << "rama_poner_cout_nuevo" << std::endl;
-
     std::cout << "commit inicial" << std::endl;
-
-    std::cout << "cambio solo en local" << std::endl;
-
 
     // Return 0 to indicate successful execution
     return 0;
