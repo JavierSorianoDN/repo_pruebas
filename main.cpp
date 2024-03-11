43c2f5ccd43914e031e43f37936a6e74d93c4446
JavierSorianoDN <javier.soriano@displaynote.com>
2024-03-11 18:05:10 +0100

    primer commit rama: rama_poner_cout_nuevo

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main() {
 
     std::cout << "commit inicial" << std::endl;
 
+    std::cout << "commit en la rama rama_poner_cout_nuevo" << std::endl;
+
     // Return 0 to indicate successful execution
     return 0;
 }
